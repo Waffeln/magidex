0764ed17aa58f0651b8851c9591d551cf298602a
waffeln <nils@kaster.space>
2023-01-18 23:08:00 +0100

    add routing structure

---
+++ src/components/FavouritePokeList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FavouritePokeList = ()=> {
+
+	return (
+		<>
+			FavouritePokeList
+		</>
+	);
+};
+
+export default FavouritePokeList;
---
+++ src/components/PokeList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PokeList = ()=> {
+
+	return (
+		<>
+			PokeList
+		</>
+	);
+};
+
+export default PokeList;
