51665be9c544ba4e7972b76b5c1a225c3441c339
waffeln <nils@kaster.space>
2023-03-09 22:16:08 +0100

    adjust header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,15 @@ import theme from "../theme";
 const Header = () => {
 
 	return <>
-		<Box sx={{height: "50px", width: "100%", backgroundColor: theme.palette.primary.main, position: "absolute", display: "flex", justifyContent: "space-around", zIndex: 200}}>
+		<Box sx={{
+			height: "50px",
+			width: "100%",
+			backgroundColor: theme.palette.primary.main,
+			display: "flex",
+			justifyContent:
+				"space-around",
+			zIndex: 200,
+		}}>
 			<Button href={`${process.env.PUBLIC_URL}/#/`}>Home</Button>
 			<Button href={`${process.env.PUBLIC_URL}/#/favourite`}>Favourites</Button>
 		</Box>
